d89674c4898c0f64de7e0cfe8c65b2190b835f4b
Shay Palachy <shaypal5@gmail.com>
2017-11-05 09:00:49 +0200

    now including subpackages correctly

--- strct/__init__.py
+++ strct/__init__.py
@@ -1,15 +1,15 @@
 """Utility pure-Python 3 decorators."""
 
-from . import dicts
-from . import lists
-from . import sets
-from . import sortedlists
-from . import general
-#import strct.dict
-#import strct.list
-#import strct.set
-#import strct.sortedlist
-#import strct.general
+#from . import dicts
+#from . import lists
+#from . import sets
+#from . import sortedlists
+#from . import general
+import strct.dicts
+import strct.lists
+import strct.sets
+import strct.sortedlists
+import strct.general
 
 from ._version import get_versions
 __version__ = get_versions()['version']
