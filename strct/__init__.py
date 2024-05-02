40f5465af7a88ce55bb21d9398a0e43e1c22d8d5
Shay Palachy <shaypal5@users.noreply.github.com>
2024-05-02 13:39:52 +0300

    init fix

--- strct/__init__.py
+++ strct/__init__.py
@@ -1,14 +1,12 @@
 """Utility pure-Python 3 decorators."""
 
+from ._version import *  # noqa: F403
+
 import strct.dicts
 import strct.lists
 import strct.sets
 import strct.sortedlists
 import strct.hash
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
 
 try:
     del strct
